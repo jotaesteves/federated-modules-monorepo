163d4ad9f5ae3901d03655b87a58bf4f2bdc3a3b
Jorge Miranda <github@litlynx.com>
2025-08-11 11:48:33 +0100

    chore: standardize doctype declaration to lowercase and format font-family in multiple HTML and CSS files

--- packages/shared/src/utils/transformations/transformations.ts
+++ packages/shared/src/utils/transformations/transformations.ts
@@ -1,5 +1,8 @@
 export const swapObjectKeyValue = (obj: Record<string, string>) =>
-  Object.keys(obj).reduce((ret, key) => {
-    ret[obj[key]] = key;
-    return ret;
-  }, {} as Record<string, string>);
+  Object.keys(obj).reduce(
+    (ret, key) => {
+      ret[obj[key]] = key;
+      return ret;
+    },
+    {} as Record<string, string>
+  );
