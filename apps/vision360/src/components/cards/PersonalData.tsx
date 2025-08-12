558025f0d619055d5f87307aa1e91a52ee2891cb
Samuel-Araujo-ITsector <samuel.araujo@itsector.pt>
2025-08-12 17:39:02 +0100

    Update apps/vision360/src/components/cards/PersonalData.tsx
    Co-authored-by: Jorge Miranda <jota.steves@gmail.com>

--- apps/vision360/src/components/cards/PersonalData.tsx
+++ apps/vision360/src/components/cards/PersonalData.tsx
@@ -3,7 +3,7 @@ import Card from 'shared/components/Card';
 export default function PersonalData() {
   return (
     <Card icon="👤" title="Dados Pessoais" className="h-full">
-      <p className='text-xs text-gray-50 '>nome completo</p>
+      <p>nome completo</p>
       <h4>Nome Sobrenome</h4>
       <br />
       <p>CIF</p>
