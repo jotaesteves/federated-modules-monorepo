eaa962d332820f459dce208f934011588257d39e
Thomas Simoes <e1669@itsector.com>
2025-08-13 17:25:34 +0100

    fix: change import of LastContact component

--- apps/vision360/src/Vision360.tsx
+++ apps/vision360/src/Vision360.tsx
@@ -4,7 +4,7 @@ import { Helmet } from 'react-helmet';
 import ChannelsAndServices from '../../vision360/src/components/cards/ChannelsAndServices';
 import EstateAndProducts from '../../vision360/src/components/cards/EstateAndProducts';
 import Incidents from '../../vision360/src/components/cards/Incidents';
-import LastContact from '../../vision360/src/components/cards/LastContact';
+import LastContact from './components/cards/LastContact/LastContact';
 import PersonalData from '../../vision360/src/components/cards/PersonalData';
 
 const Vision360: React.FC = () => {
@@ -14,25 +14,25 @@ const Vision360: React.FC = () => {
         <title>Visão 360</title>
       </Helmet>
       <div className="p-2">
-        <div className="grid grid-cols-5 grid-rows-4 gap-2">
+        <div className="grid grid-cols-4 grid-rows-4 gap-2">
           {/* div1 */}
           <div className="row-span-4">
             <PersonalData />
           </div>
           {/* div2 */}
-          <div className="col-span-3 row-span-2">
+          <div className="col-span-2 row-span-2">
             <EstateAndProducts />
           </div>
           {/* div3 */}
-          <div className="col-start-5 row-span-2">
+          <div className="col-start-4 row-span-2">
             <LastContact />
           </div>
           {/* div4 */}
-          <div className="col-span-3 row-span-2 col-start-2 row-start-3">
+          <div className="col-span-2 row-span-2 col-start-2 row-start-3">
             <ChannelsAndServices />
           </div>
           {/* div5 */}
-          <div className="col-start-5 row-start-3 row-span-2">
+          <div className="col-start-4 row-start-3 row-span-2">
             <Incidents />
           </div>
         </div>
