b7d085c9b804087cacbc63ae84b1609eb5d7ecbf
Samuel Araújo <E1670@itsector.com>
2025-08-14 18:04:21 +0100

    style:  ActivesSection, AssetsColumns, AssetsHeader, and PassivesSection to match figma

--- apps/assetsProducts/src/components/AssetsHeader/AssetsHeader.tsx
+++ apps/assetsProducts/src/components/AssetsHeader/AssetsHeader.tsx
@@ -7,9 +7,9 @@ interface AssetsHeaderProps {
 
 export const AssetsHeader: React.FC<AssetsHeaderProps> = ({ leftTitle, rightTitle }) => {
   return (
-    <div className="col-span-2 mb-2 sticky top-0 left-0 right-0 z-10 ">
+    <div className="mb-2 sticky top-0 left-0 right-0 z-10">
       <div className="bg-gradient-to-r from-primary-500 to-[#A03996] text-white p-1">
-        <div className="flex items-center px-[26px]">
+        <div className="flex items-center px-[19px]">
           <h2 className="text-lg font-semibold flex-1 text-left">{leftTitle}</h2>
           <div className="h-6 border-r border-white"></div>
           <h2 className="text-lg font-semibold flex-1 text-left pl-[26px]">{rightTitle}</h2>
