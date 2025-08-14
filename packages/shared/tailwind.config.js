1cf236880d80435de35b887d9b4a131ce2dc1327
Samuel Araújo <E1670@itsector.com>
2025-08-14 17:26:40 +0100

    fix: remove unused scrollbar plugin from tailwind configuration

--- packages/shared/tailwind.config.js
+++ packages/shared/tailwind.config.js
@@ -50,7 +50,6 @@ module.exports = {
           950: '#210112',
           DEFAULT: '#d70664',
         },
-
         gray: {
           100: '#F3F3F8',
           200: '#E5E7EB', // used
@@ -126,29 +125,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    /*
-    function ({addUtilities}){
-      const newUtilities = {
-        ".scrollbar.thin" : {
-          scrollbarWidth: "thin",
-          scrollbarColor: "#D1005D"
-        },
-        ".scrollbar-webkit": {
-          "&::-webkit-scrollbar" : {
-            width: "4px"
-          },
-          "&::-webkit-scrollbar-track" : {
-            background: "#f1f1f1"
-          },
-          "&::-webkit-scrollbar-thumb" : {
-            backgroundColor: "#D1005D",
-            borderRadius: "20px",
-            border: "1px solid white"
-          },
-        }
-      }
-      addUtilities(newUtilities, ["responsive", "hover"])
-    } */
-  ],
+  plugins: [],
 };
