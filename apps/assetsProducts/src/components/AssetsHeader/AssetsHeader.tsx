b0434f1bb615d55b175de93ad96b8013bd2dcda4
Samuel Araújo <E1670@itsector.com>
2025-08-14 17:27:17 +0100

    style: update gradient background in AssetsHeader component

--- apps/assetsProducts/src/components/AssetsHeader/AssetsHeader.tsx
+++ apps/assetsProducts/src/components/AssetsHeader/AssetsHeader.tsx
@@ -8,7 +8,7 @@ interface AssetsHeaderProps {
 export const AssetsHeader: React.FC<AssetsHeaderProps> = ({ leftTitle, rightTitle }) => {
   return (
     <div className="col-span-2 mb-2 sticky top-0 left-0 right-0 z-10 ">
-      <div className="bg-gradient-to-r from-primary-500 via-primary-700 to-purple-800 text-white p-1 rounded-t-2xl">
+      <div className="bg-gradient-to-r from-primary-500 to-[#A03996] text-white p-1">
         <div className="flex items-center px-[26px]">
           <h2 className="text-lg font-semibold flex-1 text-left">{leftTitle}</h2>
           <div className="h-6 border-r border-white"></div>
