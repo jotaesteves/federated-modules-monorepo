33ab2545c7e37279e0d638df4af66c86e1206bb4
Thomas Simoes <e1669@itsector.com>
2025-08-14 11:23:36 +0100

    feat: import LineBreak to LastContact card

--- apps/vision360/src/components/cards/LastContact/LastContact.tsx
+++ apps/vision360/src/components/cards/LastContact/LastContact.tsx
@@ -1,4 +1,5 @@
 import CardTabs from 'shared/components/CardTabs';
+import LineBreak from 'shared/components/LineBreak';
 import { CardAccordionItemContacts } from 'src/components/cards/LastContact/components/CardAccordionItemContacts';
 import type { CardTabItem } from 'shared/components/CardTabs';
 import {
@@ -17,9 +18,7 @@ const tabs: CardTabItem[] = [
         {CardAccordionItemContactsMapData.map((props, index) => (
           <React.Fragment key={index}>
             <CardAccordionItemContacts {...props} />
-            {index < CardAccordionItemContactsMapData.length - 1 && (
-              <hr className="my-2 text-gray-100" />
-            )}
+            {index < CardAccordionItemContactsMapData.length - 1 && <LineBreak />}
           </React.Fragment>
         ))}
       </>
@@ -33,7 +32,7 @@ const tabs: CardTabItem[] = [
         {CardItemMessagesMapData.map((props, index) => (
           <React.Fragment key={index}>
             <CardItemMessages {...props} />
-            {index < CardItemMessagesMapData.length - 1 && <hr className="my-2 text-gray-100" />}
+            {index < CardItemMessagesMapData.length - 1 && <LineBreak />}
           </React.Fragment>
         ))}
       </>
