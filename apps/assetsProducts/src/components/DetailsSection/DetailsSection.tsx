89788941fc3d762789ddfc3fef7fe43ba8bda9bf
Samuel Araújo <E1670@itsector.com>
2025-08-14 15:48:30 +0100

    refactor: enhance layout and styling of DetailsSection component

--- apps/assetsProducts/src/components/DetailsSection/DetailsSection.tsx
+++ apps/assetsProducts/src/components/DetailsSection/DetailsSection.tsx
@@ -8,12 +8,16 @@ interface DetailsSectionProps {
 
 export const DetailsSection: React.FC<DetailsSectionProps> = ({ title = 'Detalhes', children }) => {
   return (
-    <div className="col-span-10 flex flex-col max-h-full min-h-0">
-      <div className="sticky top-0 left-0 right-0 z-10 bg-gray-100 p-[4px] rounded-t-xl border-b border-primary-500 flex-shrink-0">
+    <div className="col-span-10 flex flex-col max-h-full min-h-0 rounded-2xl overflow-hidden ">
+      <div className="sticky top-0 left-0 right-0 z-10 bg-[#E7E9EA] py-[4px] border-b border-primary-500 flex-shrink-0 px-[26px]">
         <h2 className="text-lg font-semibold text-left">{title}</h2>
       </div>
-      <AssetsBreadcrumbs />
-      <section className="bg-white flex-1 overflow-y-auto min-h-0">{children}</section>
+      <div className="bg-white px-5 py-1">
+        <AssetsBreadcrumbs />
+      </div>
+      <section className="bg-white flex-1 overflow-y-auto min-h-0 scroll-custom">
+        {children}
+      </section>
     </div>
   );
 };
