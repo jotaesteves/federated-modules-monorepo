e49bfa91cc74ad450ecbc680e5438ec6a90d5bdd
Thomas Simoes <e1669@itsector.com>
2025-08-18 10:51:52 +0100

    feat: add data as props on CardItemIncidents

--- apps/vision360/src/components/cards/Incidents/components/CardItemIncidents.tsx
+++ apps/vision360/src/components/cards/Incidents/components/CardItemIncidents.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 import Icon from 'shared/components/Icon';
 
 interface CardItemIncidentsProps {
+  date: string;
   type: string;
   title: string;
   link: string;
 }
 
-const CardItemIncidents: React.FC<CardItemIncidentsProps> = ({ type, title, link }) => {
+const CardItemIncidents: React.FC<CardItemIncidentsProps> = ({ date, type, title, link }) => {
   return (
     <div className="flex gap-2 p-2">
       <Icon type="exclamation" className="bg-red" rounded size="sm" />
       <div className="flex flex-col gap-1">
-        <span className="font-bold text-gray-800 leading-[100%]">22-04-2025 | {type}</span>
+        <span className="font-bold text-gray-800 leading-[100%]">
+          {date} | {type}
+        </span>
         <p className="text-gray-800 font-medium leading-[100%]">{title}</p>
       </div>
       <Icon type="chevronRight" className="ml-auto" />
--- apps/vision360/src/components/cards/Incidents/mockData/mockData.tsx
+++ apps/vision360/src/components/cards/Incidents/mockData/mockData.tsx
@@ -21,16 +21,19 @@ export const CardAccordionItemClaimsMapData: CardAccordionItemClaimsProps[] = [
 
 export const CardItemIncidentsMapData = [
   {
+    date: '22-04-2025',
     type: 'INIBCHQS',
     title: 'Conta inibida de requisitar cheques',
     link: '/incident1',
   },
   {
+    date: '22-04-2025',
     type: 'MEMO',
     title: 'Conta sem assinatura digitalizada',
     link: '/incident2',
   },
   {
+    date: '22-04-2025',
     type: 'INIBCHQS',
     title: 'Conta inibida de requisitar cheques',
     link: '/incident3',
