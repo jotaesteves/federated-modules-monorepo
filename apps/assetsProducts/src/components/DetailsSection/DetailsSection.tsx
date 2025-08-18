4105a83d9986cec5413b0fb603eff20c1beba23d
Samuel Araújo <E1670@itsector.com>
2025-08-18 11:04:42 +0100

    style: update background color in DetailsSection and fix scroll class name

--- apps/assetsProducts/src/components/DetailsSection/DetailsSection.tsx
+++ apps/assetsProducts/src/components/DetailsSection/DetailsSection.tsx
@@ -9,13 +9,13 @@ interface DetailsSectionProps {
 export const DetailsSection: React.FC<DetailsSectionProps> = ({ title = 'Detalhes', children }) => {
   return (
     <div className="col-span-10 flex flex-col max-h-full min-h-0 rounded-[20px] overflow-hidden ">
-      <div className="sticky top-0 left-0 right-0 z-10 bg-[#E7E9EA] py-[4px] border-b border-primary-500 flex-shrink-0 px-[26px]">
+      <div className="sticky top-0 left-0 right-0 z-10 bg-gray-300 py-[4px] border-b border-primary-500 flex-shrink-0 px-[26px]">
         <h2 className="text-lg font-semibold text-left">{title}</h2>
       </div>
       <div className="bg-white px-5 py-1">
         <AssetsBreadcrumbs />
       </div>
-      <section className="bg-white flex-1 overflow-y-auto min-h-0 scroll-custom">
+      <section className="bg-white flex-1 overflow-y-auto min-h-0 scroll-custom-color">
         {children}
       </section>
     </div>
