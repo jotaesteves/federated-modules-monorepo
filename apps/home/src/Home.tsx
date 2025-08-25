0e3baed0fd1c665a2486b687ae98289e9ce81246
Jorge Miranda <github@litlynx.com>
2025-08-25 13:59:14 +0100

    feat(components): adds checkbox component

--- apps/home/src/Home.tsx
+++ apps/home/src/Home.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
 import ErrorBoundary from 'shared/components/ErrorBoundary';
-import { Checkbox } from 'shared/components/ui';
 
 const Home: React.FC = () => {
   const handleError = (error: Error, errorInfo: React.ErrorInfo) => {
@@ -15,7 +14,6 @@ const Home: React.FC = () => {
       </Helmet>
       <div className="grid grid-cols-24 grid-rows-10 gap-4 px-4 py-5 rounded-lg bg-gray-100 w-full overflow-y-auto h-[calc(100vh_-_194px_-_107px)]">
         This is a homepage
-        <Checkbox />
       </div>
     </ErrorBoundary>
   );
