7b9b6df1730af1bef1adb77a5088bc91799e70ac
Samuel Araujo <vsamuelparaujo@gmail.com>
2025-08-25 15:21:12 +0100

    feat: add new sidebar pages and update routing

--- apps/main/src/pages/Dashboard/index.ts
+++ apps/main/src/pages/Dashboard/index.ts
@@ -1,7 +1,5 @@
 export { Vision360Page } from './Vision360Page';
 export { PersonalDataPage } from './PersonalDataPage';
 export { AssetsProductsPage } from './AssetsProductsPage';
-export { RecordsPage } from './RecordsPage';
-export { SalesPage } from './SalesPage';
 export { ChannelsAndServicesPage } from './ChannelsAndServicesPage';
 export { default as HistoryInteractionsPage } from './HistoryInteractionsPage';
---
+++ apps/main/src/pages/Sidebar/OutboundsPage.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Spinner from 'shared/components/Spinner';
+
+const Outbounds = React.lazy(() => import('outbounds/Outbounds'));
+
+export const OutboundsPage: React.FC = () => {
+  return (
+    <React.Suspense fallback={<Spinner />}>
+      <Outbounds />
+    </React.Suspense>
+  );
+};
+
+export default OutboundsPage;
