ae611b57d600f9fbc23347aa4e40f3be71f0c8c4
Samuel Araujo <vsamuelparaujo@gmail.com>
2025-08-26 19:40:12 +0100

    feat: remove unused Doubts and Errors components

--- apps/records/src/pages/DigitalChannels/BimLine/Errors.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-export const Errors: React.FC = () => {
-  return (
-    <React.Suspense fallback={<div>Loading Errors...</div>}>
-      <h1>Errors from BimLine</h1>
-    </React.Suspense>
-  );
-};
-export default Errors;
