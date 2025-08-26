651d7faddbfdadeabf6d64abbcaf395f5f151083
Samuel Araujo <vsamuelparaujo@gmail.com>
2025-08-26 19:05:47 +0100

    feat: add sidebar paths and labels for records submenus

--- apps/header/src/data/menuData.ts
+++ apps/header/src/data/menuData.ts
@@ -33,7 +33,7 @@ export const sidebarMapData: SidebarItem[] = [
   {
     id: 'documentacao',
     icon: 'files',
-    label: 'Documentação',
+    label: 'documentation',
   },
   {
     id: 'kpis',
